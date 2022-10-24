7b73bea7e79d70fc4c630ca664a343054ca4f5c7
1Tatsumaru1 <a.ledouguet@gmail.com>
2022-10-24 23:30:16 +0200

    Reading attemps from CSV file via input binding

--- make_recommendation/__init__.py
+++ make_recommendation/__init__.py
@@ -1,16 +1,30 @@
 import logging
 
+from azure.storage.blob import BlobServiceClient, BlobClient
 import azure.functions as func
 import pandas as pd
 import numpy as np
 import json
 import os
+import io
 
 
-connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+###############################################################################################
 
+#connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+#blob_client = BlobClient.from_connection_string(connect_str)
+#blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+#container_client = blob_service_client.create_container(container_name)
+#blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+#with open(upload_file_path, "rb") as data:
+#    blob_client.upload_blob(data)
 
-def main(req: func.HttpRequest, artDf) -> func.HttpResponse:
+#download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
+#container_client = blob_service_client.get_container_client(container= container_name) 
+
+##################################################################################################
+
+def main(req: func.HttpRequest, artDf: func.InputStream) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -22,7 +36,7 @@ def main(req: func.HttpRequest, artDf) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    art_df = pd.read_csv(artDf)
+    art_df = pd.read_csv(io.StringIO(artDf.read().decode('utf-8')))
 
     if name:
         return func.HttpResponse(f"Hello, {json.dumps(art_df.iloc[0, :].to_list())}.")
