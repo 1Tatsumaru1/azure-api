46251e439ac081242b25ff533ad601a334195c65
1Tatsumaru1 <a.ledouguet@gmail.com>
2022-10-24 16:22:25 +0200

    "First commit for test function"

---
+++ make_recommendation/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+import azure.functions as func
+import pandas as pd
+import numpy as np
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        test = pd.DataFrame({'name': [name], 'value': ['1']})
+        return func.HttpResponse(f"Hello, {json.dumps(test.iloc[0, :].to_list())}. This HTTP triggered function executed successfully.")
+    else:
+        test = np.array(['status', '200'])
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
